2df722ecccebcf7da0140a06aae566e6882ac8f8
Engc90 <91764586+engc90@users.noreply.github.com>
2024-06-28 09:03:15 -0300

    Update database.py
    Se realiza la conecxión a base de datos en MongoDB

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
+from pymongo import MongoClient
+import certifi
 
+MONGO_URI = 'mongodb+srv://engc90:engc1990@cluster0.1vaz5b9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+ca = certifi.where()
+
+def dbConnection():
+    try:
+        client = MongoClient(MONGO_URI, tlsCAFile=ca)
+        db = client["dbb_products_app"]
+    except ConnectionError:
+        print('Error de conexion con la base de Base de datos')
+    return db
